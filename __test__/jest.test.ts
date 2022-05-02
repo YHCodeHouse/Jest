2d52e9a7a473c9c49dce5d986ddeb2135ae59532
김영호 <>
2022-05-02 14:06:05 +0900

    add fetch graphql

--- __test__/jest.test.ts
+++ __test__/jest.test.ts
@@ -1,5 +1,7 @@
+import "isomorphic-fetch";
 import connectDb from "../models/api/api";
 import models from "../modelSchema";
+
 describe("insert", () => {
   beforeAll(async () => {
     await connectDb();
@@ -24,9 +26,44 @@ describe("insert", () => {
       })
       .then(res => JSON.parse(JSON.stringify(res)));
 
-    console.log("mockUser ::::::::::::::::::: ", mockUser);
-    console.log("insertedusers ::::::::::::::::::: ", insertedUser);
-
     expect(insertedUser).toEqual(mockUser);
+  });
+
+  test("the stop is Arkadian puisto", async () => {
+    // The result we are expecting from the GraphQL API
+    const arkP = {
+      getHello: {
+        createdAt: "2022-03-17T04:26:53.344Z",
+        hospitalId: "vatech",
+        id: "e2e8f332-3d5e-4e87-806e-f3259f63fb38",
+        text: "aaaaa"
+      }
+    };
+
+    // The URL of the GraphQL API server
+    return (
+      fetch("http://localhost:8000/graphql", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        // The query we are sending to the GraphQL API
+        body: JSON.stringify({
+          query: `query {
+                getHello(hospitalId:"vatech",id: "e2e8f332-3d5e-4e87-806e-f3259f63fb38") {
+                    hospitalId
+                    id
+                    text
+                    createdAt
+                }
+            }`
+        })
+      })
+        .then(async res => {
+          const ret = await res.json();
+
+          return ret;
+        })
+        // The test condition itself
+        .then(res => expect(res.data).toStrictEqual(arkP))
+    );
   });
 });
